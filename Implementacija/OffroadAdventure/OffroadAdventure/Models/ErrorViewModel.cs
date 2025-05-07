2e46e921fc47a8cc57e3cdb66ab85b82c1c66acc
sjamakovic1 <sjamakovic1@etf.unsa.ba>
2025-05-07 02:26:05 +0200

    Default

---
+++ Implementacija/OffroadAdventure/OffroadAdventure/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OffroadAdventure.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
